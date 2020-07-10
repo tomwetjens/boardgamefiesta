01d1e12c99d4af597f60c1947a17eca0ea13b019
Tom Wetjens <tomwetjens@gmail.com>
2020-07-10 21:16:03 +0200

    fix compile

--- gwt/src/main/java/com/tomsboardgames/gwt/PlayerState.java
+++ gwt/src/main/java/com/tomsboardgames/gwt/PlayerState.java
@@ -14,6 +14,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Builder(access = AccessLevel.PACKAGE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PlayerState {
 
