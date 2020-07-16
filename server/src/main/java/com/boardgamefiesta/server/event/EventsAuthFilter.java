82d591b2b85422c3da82084f8292251b12ba9149
Tom Wetjens <tomwetjens@gmail.com>
2020-07-16 11:49:10 +0200

    prevent unnecessary logging

--- server/src/main/java/com/boardgamefiesta/server/event/EventsAuthFilter.java
+++ server/src/main/java/com/boardgamefiesta/server/event/EventsAuthFilter.java
@@ -2,6 +2,7 @@ package com.boardgamefiesta.server.event;
 
 import io.quarkus.oidc.IdTokenCredential;
 import io.quarkus.oidc.runtime.OidcIdentityProvider;
+import io.quarkus.security.AuthenticationFailedException;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.security.identity.request.TokenAuthenticationRequest;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.concurrent.CompletableFuture;
@@ -26,24 +28,34 @@ public class EventsAuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
 
         var token = httpServletRequest.getParameter("token");
 
-        if (token != null && !"".equals(token)) {
-            try {
-
-                IdTokenCredential idTokenCredential = new IdTokenCredential(token, null);
-                TokenAuthenticationRequest tokenAuthenticationRequest = new TokenAuthenticationRequest(idTokenCredential);
-
-                SecurityIdentity securityIdentity = oidcIdentityProvider.authenticate(tokenAuthenticationRequest, function -> CompletableFuture.completedFuture(function.get()))
-                        .toCompletableFuture().get();
-
-                filterChain.doFilter(new AuthenticatedRequest(httpServletRequest, securityIdentity.getPrincipal()), servletResponse);
-            } catch (InterruptedException | ExecutionException e) {
-                log.error("Websockets auth filter error", e);
-            }
-        } else {
+        if (token == null || "".equals(token)) {
             filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        try {
+            IdTokenCredential idTokenCredential = new IdTokenCredential(token, null);
+            TokenAuthenticationRequest tokenAuthenticationRequest = new TokenAuthenticationRequest(idTokenCredential);
+
+            SecurityIdentity securityIdentity = oidcIdentityProvider.authenticate(tokenAuthenticationRequest, function -> CompletableFuture.completedFuture(function.get()))
+                    .toCompletableFuture().get();
+
+            filterChain.doFilter(new AuthenticatedRequest(httpServletRequest, securityIdentity.getPrincipal()), servletResponse);
+        } catch (ExecutionException e) {
+            if (e.getCause() instanceof AuthenticationFailedException) {
+                log.debug("Authentication failed", e);
+                httpServletResponse.sendError(401);
+            } else {
+                log.error("Unexpected error during authentication", e);
+                httpServletResponse.sendError(500);
+            }
+        } catch (InterruptedException e) {
+            log.debug("Interrupted while authenticating", e);
+            httpServletResponse.sendError(401);
         }
     }
 
