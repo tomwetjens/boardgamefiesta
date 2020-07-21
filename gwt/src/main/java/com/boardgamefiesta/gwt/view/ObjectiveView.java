de046c9132e490b0f99ac084d1746195f995ebdc
Tom Wetjens <tomwetjens@gmail.com>
2020-07-21 14:27:43 +0200

    remove deprecated fields

--- gwt/src/main/java/com/boardgamefiesta/gwt/view/ObjectiveView.java
+++ gwt/src/main/java/com/boardgamefiesta/gwt/view/ObjectiveView.java
@@ -1,9 +1,6 @@
 package com.boardgamefiesta.gwt.view;
 
-import com.boardgamefiesta.gwt.logic.ObjectiveCard;
 import lombok.Value;
-
-import java.util.List;
 
 @Value
 public class ObjectiveView implements Comparable<ObjectiveView> {
@@ -11,20 +8,9 @@ public class ObjectiveView implements Comparable<ObjectiveView> {
     ObjectiveCardView objectiveCard;
     int score;
 
-    // TODO For backwards compatibility, remove asap
-    int points;
-    int penalty;
-    List<ObjectiveCard.Task> tasks;
-    ActionType action;
-
     ObjectiveView(ObjectiveCardView objectiveCard, int score) {
         this.objectiveCard = objectiveCard;
         this.score = score;
-
-        this.points = objectiveCard.getPoints();
-        this.penalty = objectiveCard.getPenalty();
-        this.tasks = objectiveCard.getTasks();
-        this.action = objectiveCard.getAction();
     }
 
     @Override
