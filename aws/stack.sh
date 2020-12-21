e3f59ee1b2e37369dca418849ed6d510e3e5b67d
Tom Wetjens <tomwetjens@gmail.com>
2020-12-21 14:18:22 +0100

    wip cloudformation

--- aws/stack.sh
+++ aws/stack.sh
@@ -5,27 +5,27 @@
 #./stack.sh update dev 294
 #./stack.sh update prod 294
 
-ACTION=$1
+ACTION=$1-stack
 ENV=$2
 VERSION=$3
 
 STACK_NAME=boardgamefiesta-$ENV
 
-echo "${ACTION} stack $STACK_NAME"
+echo "${ACTION}: $STACK_NAME"
 
-aws cloudformation $ACTION-stack --stack-name $STACK_NAME-vpc \
+aws cloudformation $ACTION --stack-name $STACK_NAME-vpc \
   --template-body file://vpc.yaml
 
-#aws cloudformation $ACTION-stack --stack-name $STACK_NAME-db \
+#aws cloudformation $ACTION --stack-name $STACK_NAME-db \
 #  --template-body file://db.yaml \
 #  --parameters ParameterKey=Suffix,ParameterValue=$SUFFIX
 
-aws cloudformation $ACTION-stack --stack-name $STACK_NAME-auth \
+aws cloudformation $ACTION --stack-name $STACK_NAME-auth \
   --template-body file://auth.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameters ParameterKey=Environment,ParameterValue=$ENV
-
-aws cloudformation $ACTION-stack --stack-name $STACK_NAME-api \
-  --template-body file://api.yaml \
-  --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-  --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=Version,ParameterValue=$VERSION
+#
+#aws cloudformation $ACTION --stack-name $STACK_NAME-api \
+#  --template-body file://api.yaml \
+#  --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+#  --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=Version,ParameterValue=$VERSION
