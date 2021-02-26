235f7be35706f74db371ce09cb1dc924f151fe57
Tom Wetjens <tomwetjens@gmail.com>
2021-02-26 15:17:34 +0100

    Add '/user' endpoint

--- server/src/main/java/com/boardgamefiesta/server/rest/user/UserResource.java
+++ server/src/main/java/com/boardgamefiesta/server/rest/user/UserResource.java
@@ -2,33 +2,22 @@ package com.boardgamefiesta.server.rest.user;
 
 import com.boardgamefiesta.domain.user.User;
 import com.boardgamefiesta.domain.user.Users;
-import com.boardgamefiesta.server.rest.exception.APIError;
-import com.boardgamefiesta.server.rest.exception.APIException;
 import com.boardgamefiesta.server.rest.user.view.UserView;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
-import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotAuthorizedException;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
-import java.time.DateTimeException;
-import java.time.ZoneId;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
-@Path("/users")
-@Consumes(MediaType.APPLICATION_JSON)
+@Path("/user")
 @Produces(MediaType.APPLICATION_JSON)
 @RolesAllowed("user")
 public class UserResource {
-
-    private static final int MIN_USERNAME_LENGTH = 3;
-    private static final int MIN_EMAIL_LENGTH = 6;
-    private static final int MAX_SEARCH_RESULTS = 5;
 
     @Inject
     Users users;
@@ -37,94 +26,9 @@ public class UserResource {
     SecurityContext securityContext;
 
     @GET
-    public List<UserView> searchUsers(@QueryParam("q") String q) {
-        if (q != null && q.contains("@") && q.length() >= MIN_EMAIL_LENGTH) {
-            return users.findByEmail(q)
-                    .map(user -> new UserView(user.getId(), user, currentUserId()))
-                    .map(Collections::singletonList)
-                    .orElse(Collections.emptyList());
-        } else if (q != null && q.length() >= MIN_USERNAME_LENGTH) {
-            return users.findByUsernameStartsWith(q)
-                    .limit(MAX_SEARCH_RESULTS)
-                    .map(user -> new UserView(user.getId(), user, currentUserId()))
-                    .collect(Collectors.toList());
-        } else {
-            throw APIException.badRequest(APIError.MUST_SPECIFY_USERNAME_OR_EMAIL);
-        }
-    }
-
-    @GET
-    @Path("/{id}")
-    public UserView get(@PathParam("id") String id) {
-        var user = users.findById(User.Id.of(id), false);
+    public UserView get() {
+        var user = users.findById(currentUserId(), false);
         return new UserView(user.getId(), user, currentUserId());
-    }
-
-    @POST
-    @Path("/{id}/change-location")
-    public void changeLocation(@PathParam("id") String id, ChangeLocationRequest request) {
-        var userId = User.Id.of(id);
-
-        checkCurrentUser(userId);
-
-        handleConcurrentModification(userId, user ->
-                user.changeLocation(request.getLocation()));
-    }
-
-    @POST
-    @Path("/{id}/change-language")
-    public void changeLanguage(@PathParam("id") String id, ChangeLanguageRequest request) {
-        var userId = User.Id.of(id);
-
-        checkCurrentUser(userId);
-
-        handleConcurrentModification(userId, user ->
-                user.changeLanguage(request.getLanguage()));
-    }
-
-    @POST
-    @Path("/{id}/change-time-zone")
-    public void changeLanguage(@PathParam("id") String id, ChangeTimeZoneRequest request) {
-        var userId = User.Id.of(id);
-
-        checkCurrentUser(userId);
-
-        try {
-            var zoneId = ZoneId.of(request.getTimeZone());
-
-            handleConcurrentModification(userId, user ->
-                    user.changeTimeZone(zoneId));
-        } catch (DateTimeException e) {
-            throw APIException.badRequest(APIError.INVALID_TIME_ZONE);
-        }
-    }
-
-    @POST
-    @Path("/{id}/change-email")
-    public void changeEmail(@PathParam("id") String id, @Valid ChangeEmailRequest request) {
-        var userId = User.Id.of(id);
-
-        checkCurrentUser(userId);
-
-        handleConcurrentModification(userId, user ->
-                user.changeEmail(request.getEmail()));
-    }
-
-    @POST
-    @Path("/{id}/change-password")
-    public void changePassword(@PathParam("id") String id, @Valid ChangePasswordRequest request) {
-        var userId = User.Id.of(id);
-
-        checkCurrentUser(userId);
-
-        handleConcurrentModification(userId, user ->
-                user.changePassword(request.getPassword()));
-    }
-
-    private void checkCurrentUser(User.Id userId) {
-        if (!userId.equals(currentUserId())) {
-            throw APIException.forbidden(APIError.NOT_SUPPORTED);
-        }
     }
 
     private User.Id currentUserId() {
@@ -133,25 +37,4 @@ public class UserResource {
         }
         return User.Id.of(securityContext.getUserPrincipal().getName());
     }
-
-    private User handleConcurrentModification(User.Id id, Consumer<User> modifier) {
-        int retries = 0;
-        do {
-            var table = users.findById(id, true);
-
-            modifier.accept(table);
-
-            try {
-                users.update(table);
-
-                return table;
-            } catch (Users.ConcurrentModificationException e) {
-                if (retries >= 1) {
-                    throw e;
-                }
-                retries++;
-            }
-        } while (true);
-    }
-
 }
