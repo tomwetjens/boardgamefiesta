4655fd4900c9f952178717bbc0f02be778dfb615
Tom Wetjens <tomwetjens@gmail.com>
2021-04-28 16:25:41 +0200

    Upload lambda code with timestamp

--- aws/stack.sh
+++ aws/stack.sh
@@ -8,6 +8,7 @@
 ACTION=$1-stack
 ENV=$2
 VERSION=$3
+TIMESTAMP=$(date -u +%FT%T.000Z)
 
 STACK_NAME=boardgamefiesta-$ENV
 
@@ -20,19 +21,19 @@ echo "${ACTION}: $STACK_NAME"
 #  --template-body file://db.yaml \
 #  --parameters ParameterKey=Suffix,ParameterValue=$SUFFIX
 
-aws s3 cp ../dynamodb-triggers/target/function.zip s3://boardgamefiesta-builds/$VERSION/dynamodb-triggers.zip
+aws s3 cp ../dynamodb-triggers/target/function.zip s3://boardgamefiesta-builds/$TIMESTAMP/dynamodb-triggers.zip
 
 aws cloudformation $ACTION --stack-name $STACK_NAME-triggers \
   --template-body file://triggers.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=Version,ParameterValue=$VERSION
+  --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=Version,ParameterValue=$TIMESTAMP
 
-#aws s3 cp ../cognito/target/function.zip s3://boardgamefiesta-builds/$VERSION/cognito.zip
+aws s3 cp ../cognito/target/function.zip s3://boardgamefiesta-builds/$TIMESTAMP/cognito.zip
 
-#aws cloudformation $ACTION --stack-name $STACK_NAME-auth \
-#  --template-body file://auth.yaml \
-#  --capabilities CAPABILITY_NAMED_IAM \
-#  --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=Version,ParameterValue=$VERSION
+aws cloudformation $ACTION --stack-name $STACK_NAME-auth \
+  --template-body file://auth.yaml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=Environment,ParameterValue=$ENV ParameterKey=Version,ParameterValue=$TIMESTAMP
 
 #aws cloudformation $ACTION --stack-name $STACK_NAME-api \
 #  --template-body file://api.yaml \
