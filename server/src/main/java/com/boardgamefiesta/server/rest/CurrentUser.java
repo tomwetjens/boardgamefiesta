06a310cf2f32ef1790d0812aefe7190bc5e41a34
Tom Wetjens <tomwetjens@gmail.com>
2021-05-29 15:26:12 +0200

    Automatically create user if not exists (primarily for dev and test)

--- server/src/main/java/com/boardgamefiesta/server/rest/CurrentUser.java
+++ server/src/main/java/com/boardgamefiesta/server/rest/CurrentUser.java
@@ -2,10 +2,10 @@ package com.boardgamefiesta.server.rest;
 
 import com.boardgamefiesta.domain.user.User;
 import com.boardgamefiesta.domain.user.Users;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.NotAuthorizedException;
 
 @RequestScoped
@@ -15,34 +15,48 @@ public class CurrentUser {
     Users users;
 
     @Inject
-    HttpServletRequest request;
+    JsonWebToken jsonWebToken;
 
     private User.Id id;
     private User user;
 
     public User.Id getId() {
         if (id == null) {
-            var principalName = currentPrincipalName();
+            var cognitoUsername = getCognitoUsername();
 
-            id = users.findIdByCognitoUsername(principalName)
-                    .orElseThrow(() -> new NotAuthorizedException("User not found"));
+            users.findIdByCognitoUsername(cognitoUsername)
+                    .ifPresentOrElse(id -> this.id = id, this::createAutomatically);
         }
         return id;
     }
 
+    private void createAutomatically() {
+        var user = User.createAutomatically(getCognitoUsername(), getEmail());
+
+        // TODO Fix race condition where multiple items are created for the same username (in case of parallel requests)
+        users.add(user);
+
+        this.user = user;
+        this.id = user.getId();
+    }
+
     public User get() {
         if (user == null) {
-            user = users.findById(getId())
-                    .orElseThrow(() -> new NotAuthorizedException("User not found"));
+            var id = getId();
+            if (user == null) {
+                user = users.findById(id)
+                        .orElseThrow(() -> new NotAuthorizedException("User not found"));
+            }
         }
         return user;
     }
 
-    private String currentPrincipalName() {
-        if (request.getUserPrincipal() == null || request.getUserPrincipal().getName() == null) {
-            throw new NotAuthorizedException("User not authenticated");
-        }
-        return request.getUserPrincipal().getName();
+    private String getCognitoUsername() {
+        return jsonWebToken.getClaim("cognito:username");
+    }
+
+    private String getEmail() {
+        return jsonWebToken.getClaim("email");
     }
 
 }
